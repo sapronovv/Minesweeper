ed6e15d347ae358a6fcfaf4be97f9c9fed3551cb
Vladimir Sapronov <vov_sapr@mail.ru>
2015-10-17 19:44:32 +0400

    some TODO

--- Saper/Program.cs
+++ Saper/Program.cs
@@ -14,9 +14,18 @@ namespace Saper
         [STAThread]
         static void Main()
         {
+            //TODO use embeded resourse to store images
+            // TODO use settings
+
+            //TODO use localizations
+
+            //TODO camel case fo local variables
+
+            // TODO Rihter
             if (!File.Exists("setup.ini"))
                 File.AppendAllText("setup.ini", "9 9 10 1");
             if (!File.Exists("records.ini"))
+                //TODO remove duplicated strings. 
                 File.AppendAllText("records.ini", "Новичок 99 99 Anonim\nОпытный 99 99 Anonim\nЭлита 99 99 Anonim");
             StreamReader fsetup = new StreamReader("setup.ini");
             string SetStr = fsetup.ReadLine();
